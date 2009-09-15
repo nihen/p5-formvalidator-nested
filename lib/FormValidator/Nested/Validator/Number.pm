42e4d0bd0a8eb257e7c8314e0035e3d1edf38576
Masahiro Chiba <nihen@megabbs.com>
2009-09-15 22:16:02 +0900

    initial import

---
+++ lib/FormValidator/Nested/Validator/Number.pm
@@ -0,0 +1,19 @@
+package FormValidator::Nested::Validator::Number;
+use strict;
+use warnings;
+use utf8;
+
+
+
+sub number {
+    my ( $value, $options, $req ) = @_;
+
+    if ( $value =~ /[^0-9]/ ) {
+        return 0;
+    }
+    return 1;
+}
+
+
+
+1;
